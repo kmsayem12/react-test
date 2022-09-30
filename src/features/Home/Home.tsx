53dae3a582465eb54c7d662125fa939f5459def5
kmsayem <sayem@strativ.se>
2022-09-30 22:41:09 +0600

    refactore and save create new user data

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,26 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './Header'
 import UserList from './UserList'
 import UserAdd from './UserAdd'
-import { useUsersQuery } from './api'
+import { getUsers } from './api'
 import { IUser } from '@/types'
 import { HomeStyles } from './styles'
 
 const Home: React.FC = () => {
   const [hasModal, setHasModal] = useState(false)
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    getUsers().then(data => {
+      setUsers(data)
+    })
+  }, [])
 
   const toggleModal = () => {
     setHasModal(!hasModal)
   }
-  const { data } = useUsersQuery()
   const addNewUser = (values: IUser) => {
-    console.log({ values })
+    setUsers(prev => [...prev, values])
+    toggleModal()
   }
+
   return (
     <HomeStyles>
       <Header toggleModal={toggleModal} />
       <div className="content">
-        <UserList list={data} />
+        <UserList list={users} />
       </div>
       <UserAdd hasModal={hasModal} toggleModal={toggleModal} addNewUser={addNewUser} />
     </HomeStyles>
--- src/features/Home/api.ts
+++ src/features/Home/api.ts
@@ -1,13 +1,10 @@
-import { useQuery } from 'react-query'
 import { userUrl } from '@/api/endpoints'
 import { http } from '@/services'
 import { IUser } from '@/types'
 
-type Users = ReadonlyArray<IUser>
+type Users = Array<IUser>
 
-const getUsers = async (): Promise<Users> => {
+export const getUsers = async (): Promise<Users> => {
   const response = await http.get(userUrl)
   return response.data
 }
-
-export const useUsersQuery = () => useQuery('getUser', () => getUsers())
