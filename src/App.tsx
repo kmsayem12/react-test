d2009d5b69b421de31c957256d899bb9a6c61a2f
kmsayem <sayem@strativ.se>
2022-10-01 00:04:44 +0600

    update title and remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
 import Layout from '@/components/Layouts'
 import { HomeScreen } from '@/screens/Home'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-})
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Layout>
-          <HomeScreen />
-        </Layout>
-      </div>
-    </QueryClientProvider>
+    <div className="App">
+      <Layout>
+        <HomeScreen />
+      </Layout>
+    </div>
   )
 }
 export default App
